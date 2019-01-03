9f8c32b80255143cf1577c15725c3ea13063c0aa
HH2233 <2573342359@qq.com>
2019-01-03 11:11:32 +0800

    123

---
+++ 1.php
@@ -0,0 +1,10 @@
+
+<?php
+header("Access-Control-Allow-Origin : http://10.30.29.76:63341");
+header("Access-Control-Allow-Credentials : true");
+header('Access-Control-Allow-Headers : Content-Type');
+
+//echo '{"userName":"123"}';
+$data=array("userName"=>"123hw");
+echo json_encode($data);
+?>
