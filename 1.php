3480904ad49bc991261eb7c118a37bf7c907d3d7
17002090234徐昭辉 <43157572+HH2233@users.noreply.github.com>
2019-01-08 09:31:14 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,10 +1,8 @@
-
 <?php
 header("Access-Control-Allow-Origin : http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
-
 //echo '{"userName":"123"}';
-$data=array("userName"=>"123hw");
+$data=array("userName"=>"xk");
 echo json_encode($data);
 ?>
