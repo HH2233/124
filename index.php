6812923dc927357eba45f136722eed2a50a960f9
17002090234徐昭辉 <43157572+HH2233@users.noreply.github.com>
2019-01-08 09:31:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+
 <?php
-header("Access-Control-Allow-Origin : http://10.30.29.56:63341");
+header("Access-Control-Allow-Origin : http://10.30.29.76:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
 
 //echo '{"userName":"123"}';
-$data=array("userName"=>"123");
+$str = file_get_contents('php://input');
+
+
+$username=json_decode($str)->userName;
+$password=json_decode($str)->passWord;
+
+if($username== '123' && $password=='123'){
+    $data=
+        ['message' => 'success'];
+}else{
+    $data=
+        ['message' => 'error'];
+}
+
+
 echo json_encode($data);
 ?>
